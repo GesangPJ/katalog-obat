c3081c11d147d412755a7f5cf668e1f9d76ad0c9
Gesang Paudra J <g.paudra@gmail.com>
2023-09-22 19:44:30 +0700

    Add MongoDB to server-side

--- src/views/statuspage/index.js
+++ src/views/statuspage/index.js
@@ -5,8 +5,7 @@ import CardHeader from '@mui/material/CardHeader'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Grid'
-
-
+import React, { useEffect, useState } from 'react';
 
 const DemoGrid = styled(Grid)(({ theme }) => ({
   [theme.breakpoints.down('sm')]: {
@@ -15,6 +14,20 @@ const DemoGrid = styled(Grid)(({ theme }) => ({
 }));
 
 const StatusPage = () => {
+  const [mongoDBStatus, setMongoDBStatus] = useState(null);
+
+  useEffect(() => {
+    // Make a GET request to the backend to check the MongoDB status
+    fetch('http://localhost:3000/api/mongodb-status')
+      .then((response) => response.json())
+      .then((data) => {
+        setMongoDBStatus(data.isConnected ? 'Connected' : 'Disconnected');
+      })
+      .catch((error) => {
+        console.error(error);
+        setMongoDBStatus('Error');
+      });
+  }, []);
 
   return (
     <Card>
@@ -29,13 +42,12 @@ const StatusPage = () => {
         <Grid container spacing={6} justifyContent="center" textAlign={'justify'}>
           <DemoGrid item xs={5} sm={9}>
             <Typography variant="body1">
-              Mongo DB Connection:
+              Mongo DB Connection : {mongoDBStatus}
             </Typography>
           </DemoGrid>
         </Grid>
       </CardContent>
-    </Card>
-  );
-};
+    </Card>)
+}
 
 export default StatusPage;
