a18610708978eb7ccde3c57ee98d3e1f0205028b
Gesang Paudra J <g.paudra@gmail.com>
2023-10-01 19:48:13 +0700

    add custom search bar

---
+++ src/layouts/components/vertical/SearchBar.js
@@ -0,0 +1,46 @@
+// SearchBar.js
+import React, { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import Magnify from 'mdi-material-ui/Magnify';
+import { Link } from 'next/link';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    // Make an API request for search results and update the state
+    // You can use axios or the fetch API for this.
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <TextField
+        label="Search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        size="small"
+        sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 } }}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Magnify fontSize="small" />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <div>
+        {searchResults.map((result) => (
+          <div key={result.namaObat}>
+            <Link href={`/detail-obat-generik?namaObat=${result.namaObat}`} passHref>
+              {result.namaObat}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
